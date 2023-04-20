fad81d0ea273b966a1f9d88ae86ded9815a7a8ea
Alexandr1200 <uchiha1200@gmail.com>
2023-04-20 22:08:18 +0300

    HW10
    Додав можливісь додавати ще номера. І додав можливість відаляння.

--- Field1.py
+++ Field1.py
@@ -1,9 +1,9 @@
 from collections import UserDict
 
 
-class Core:
+class Field:
     
-    def __init__(self, value):
+    def __init__(self, value=None):
         if not isinstance(value, str):
             raise ValueError("Value must be a string")
         self.value = value
@@ -11,24 +11,37 @@ class Core:
     def __str__(self) -> str:
         return str(self.value)
 
-class Name(Core):
-    pass
-
-class Phone(Core):
-    pass
-
-class Record:
+    def __repr__(self) -> str:
+        return str(self)
     
-    def __init__(self, name:Name, phone:Phone):
+class Name(Field):
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if not isinstance(value, int):
+            raise ValueError('Must be a interger')
+        self.value = value
+
+class Record(Field):
+    
+    def __init__(self, name, phone):
         self.name = name
-        self.phone = phone
+        self.phones = [phone] if phone else []
       
-    def change_phone(self, new_phone):
-        old_phone = self.phone
-        self.phone = new_phone
+    def change_phone(self, old_phone, new_phone):
+        for np, op in enumerate(self.phones):
+            if op.value == old_phone.value:
+                self.phones[np] = new_phone
         return f"Change {old_phone} to {new_phone}"
+    
+    def add_phone(self, phone):
+        self.phones.append(phone)
+
+    def del_phone(self, phone):
+        self.phones.remove(phone)
         
-class AddressBook(UserDict):
+class AdressBook(UserDict):
     
     def add_record(self, record:Record):
         self.data[record.name.value] = record
@@ -36,16 +49,16 @@ class AddressBook(UserDict):
 
 if __name__ == '__main__':
     
-    addressbook = AddressBook()
+    addressbook = AdressBook()
     
-    name = Name("Alex")
-    phone = Phone("9999999999")
+    name = Name('Alex')
+    phone = Phone(9999999999)
     
     record1 = Record(name, phone)
-    
-    print(f"{record1.name} - {record1.phone}")
+    record1.add_phone(Phone(123456))
+    print(f"{record1.name} - {record1.phones}")
      
-    record2 = Record(Name('Igor'), Phone("9999999990"))
+    record2 = Record(Name('Igor'), Phone(9999999990))
     
     addressbook.add_record(record1)
     addressbook.add_record(record2)
@@ -53,8 +66,10 @@ if __name__ == '__main__':
     record3 = addressbook.get('Alex')
 
     if record3:
-        record3.change_phone(Phone('9999999900'))
+        record3.change_phone(Phone(9999999999), Phone(9999999900))
     
     for record in addressbook.values():
-        print(f"{record.name} {record.phone}")
+        print(f"{record.name} {record.phones}")
+
+    
     
--- Field2.py
+++ Field2.py
@@ -1,35 +1,110 @@
-from Field1 import AddressBook, Record, Name, Phone
+from Field1 import AdressBook, Record, Name, Phone
 
 
-addressbook = AddressBook()
+adressbook = AdressBook()
+
+contacts = {'Alina': ['+290424'],
+            'Ira': ['+3425590', '+15305383'],
+            'Vitalina': ['+14293798']}
+
+def input_error(func):
+    def inner(*args):
+        try:
+            return func(*args)
+        except IndexError:
+            return 'Something wrong. Type help.'
+    return inner
+
+@input_error
+def list_of_params(*args):
+    conteiner = args[0].split()
+    if not conteiner:
+        raise IndexError
+    return conteiner
+
+def help(*args):
+    return """
+Show all contacts >> input show_all
+Add new contact >>>> input add
+Get contact >>>>>>>> input phone (Name)
+Change number >>>>>> input change_contact (Name) (current number) (new number)
+Remove contact >>>>> input remove (Name)
+For exit ----------- enter exit
+"""
+
+def hello(*args):
+    return 'For information - input help'
+
+@input_error
+def add(*args):
+    lst = list_of_params(*args)
+    if len(lst) == 2:
+        name = Name(lst[0])
+        numb_of_phone = Phone(int(lst[1]))
+        new_contact = Record(name, numb_of_phone)
+        adressbook.add_record(new_contact)
+    else:
+        raise IndexError
 
 
-def add_record(*args):
-    name = Name(args[0])
-    phone = Phone(args[1])
-    record = Record(name, phone)
-    addressbook.add_record(record)
-    return f"Name {name} add successful"
+def exit(*args):
+    return 'Bye'
 
+def no_command(*args):
+    return 'Unknown command. Try again'
+
+def show_all(*args):
+    return '\n'.join([f'{k}: {v.phones}' for k, v in adressbook.items()])
+
+@input_error
+def get_number(*args):
+    lst = list_of_params(*args)
+    for k, v in adressbook.items():
+        if lst[0] == k:
+            return f'{lst[0]}: {v.phones}'
+    return f'Not contacts {lst[0]}'
+
+@input_error
+def change_contact(*args):
+    lst = list_of_params(*args)
+    if len(lst) == 2:
+        for k, v in adressbook.items():
+            if k == lst[0]:
+                contact = Record(Name[k], Phone(int(lst[1])))
+                adressbook.update({contact.name.value: contact})
+        return f'Contact {lst[0]} changed'
+    else:
+        raise IndexError
+
+def remove_contact(self, name):
+    self.data.pop(name)
+
+COMMANDS = {help: 'help',
+            add: 'add',
+            exit: 'exit',
+            show_all: 'show_all',
+            get_number: 'phone',
+            change_contact: 'change_contact',
+            remove_contact: 'remove',
+            hello: 'hello'}
+
+
+def command_handler(text: str):
+    for command, kword in COMMANDS.items():
+        if text.startswith(kword):
+            return command, text.replace(kword, '').strip()
+    return no_command, None
 
 def main():
     while True:
-        user_input = input(">>>")
-        
-        if user_input.startswith("add"):
-            name, phone = user_input.replace("add", '').strip().split()
-            print(add_record(name, phone))
 
-        elif user_input != "exit":
-            print("Unknown command, try again")
-            
-        if user_input.startswith("exit"):
-            print("Bye")
+        user_input = input('>>> ')
+        if user_input in ('good bye', 'close', 'exit'):
+            print(exit())
             break
-        
+        command, data = command_handler(user_input)
+        print(command(data))
 
 if __name__ == '__main__':
 
     main()
-    for record in addressbook.values():
-        print(f"{record.name} - {record.phone}")
